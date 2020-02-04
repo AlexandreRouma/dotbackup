5ce5fff68dbc0ba0bd04f7d4f90486057a41944e
AlexandreRouma <alexandre.rouma@gmail.com>
2020-02-04 21:15:21 +0100

    New edit

--- install_base.sh
+++ install_base.sh
@@ -1,1 +1,1 @@
-yay -S i3wm polybar netctl betterlockscreen bluez bluez-utils bluez-tools bluez-alsa-git pulseaudio pulseaudio-alsa
+yay -S ttf-inconsolata rxvt-unicode i3wm polybar netctl betterlockscreen bluez bluez-utils bluez-tools bluez-alsa-git pulseaudio pulseaudio-alsa
